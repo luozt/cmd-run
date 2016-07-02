66da31ef00cc78231f3bea64f56776196f13dc26
coluozhitao <luozt@foxmail.com>
2016-07-02 18:07:38 +0800

    cmd-run birth. feature: http-server -a 127.0.0.1 -p 8001

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # cmd-run
-run cmd as a shortcut
+
+## summary
+
+Run some common commands as a short cut here using `cmd-run` npm module to save developing steps and time.
+
+## usage
+
+use command: `cmd-run <command>`
+
+Currently the most common command I am using is `http-server -a 127.0.0.1 -p 8001`.
+
+I put it in `cmd-run` command list and this list will grow gradually.
+
+`<commnad>` list:
+
+* http-server: `http-server -a 127.0.0.1 -p 8001`
+
+
---
+++ bin/cmd-run.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+var http = require("http");
+var argv = require("minimist")(process.argv.slice(2));
+
+var config = require('../package.json').scripts;
+
+var arrCmd = argv['_'] || [];
+var cmd;
+
+arrCmd.forEach(function(cmdKey){
+  cmd = config[cmdKey];
+
+  if(cmd){
+    var childProcess = require("child_process");
+    var exec = childProcess.exec;
+
+    console.log("cmd get [ " + cmd + " ]");
+
+    var child = exec('npm run ' + cmdKey, function(err, stdout, stderr){
+      if(err){
+        console.log("cmd [ "+ cmd +" ] err: " + err);
+        return;
+      }
+
+      console.log("cmd [ " + cmd + " ] success");
+    });
+
+
+    switch(cmdKey){
+      case 'http-server':
+        openUrl('http://127.0.0.1:8001/', 8001);
+        break;
+    }
+
+  }
+});
+
+
+/**
+ * 打开浏览器
+ */
+function openUrl (path, callback) {
+  var IS_WIN = process.platform.indexOf('win') === 0;
+  var escapeShellArg = function(cmd) {
+    return '"' + cmd + '"';
+  };
+
+  var child_process = require("child_process");
+  var cmd = escapeShellArg(path);
+  if (IS_WIN) {
+    cmd = 'start "" ' + cmd;
+  } else {
+    if (process.env["XDG_SESSION_COOKIE"] ||
+      process.env["XDG_CONFIG_DIRS"] ||
+      process.env["XDG_CURRENT_DESKTOP"]) {
+      cmd = "xdg-open " + cmd
+    } else if (process.env["GNOME_DESKTOP_SESSION_ID"]) {
+      cmd = 'gnome-open ' + cmd;
+    } else {
+      cmd = "open " + cmd;
+    }
+  }
+  child_process.exec(cmd, callback);
+}
