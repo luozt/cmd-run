1974026f544bd8d735e389a5778d7e351ee49249
luozhitao <hello@luozhitaodeMacBook-Air.local>
2016-10-23 15:06:49 +0800

    add, delete, view feature

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Run some common commands as a short cut here using `cmd-run` npm module to save 
 
 ## installation
 
-It needs to be installed globally: `npm i cmd-run -g`
+**Only in Windows**, it can be installed globally: `npm i cmd-run -g`.
+
+In MacOS, it can only be used locally: `npm i cmd-run`, `node ./bin/cmd-run.js http-server`
 
 ## usage
 
@@ -16,11 +18,23 @@ Currently the most common command I am using is `http-server -a 127.0.0.1 -p 800
 
 I put it in `cmd-run` command list and this list will grow gradually.
 
-`<commnad>` list:
+Default `<commnad>` list:
 
 * `http-server`: http-server -a 127.0.0.1 -p 8001
 * `hs`: http-server -p 8001
 * `gf-push`: git push origin HEAD:refs/heads/master
 * `clear.npm`: help you remove and clear the current working path's `node_modules` dir
 
+## new features from v1.1.0
 
+Now `cmd-run` supports command list and view, add, delete and so on, as the followings:
+
+* `cmd-run -a push-to-test -v "git push origin HEAD:refs/heads/test"`: add `push-to-test` as the shortcut of `git push origin HEAD:refs/heads/test`
+* `cmd-run -d push-to-test`: just delete `push-to-test` command what you just add
+* `cmd-run -l`: list the current `cmd-run` config
+
+## TODOS
+
+* `cmd-run` now doesn't support MacOS.
+
+Welcome to make contribution for `cmd-run` at `https://github.com/luozt/cmd-run.git`.
--- bin/cmd-run.js
+++ bin/cmd-run.js
@@ -2,59 +2,133 @@
 
 var http = require("http");
 var path = require("path");
+var fs = require('fs');
 var argv = require("minimist")(process.argv.slice(2));
+var configFilePathFull = path.join(__dirname, '../config.json');
+var config = require(configFilePathFull);
 
-var config = require(path.join(__dirname, '../config.json'));
+onInit();
 
-var arrCmd = argv['_'] || [];
-var cmd;
+/** 初始化 */
+function onInit(){
+  if(argv['_'].length){
+    cmdRun();
+  }else{
+    cmdConfig();
+  }
+}
 
-arrCmd.forEach(function(cmdKey){
-  cmd = config[cmdKey];
+/** 命令配置  */
+function cmdConfig(){
+  var config = getConfig();
 
-  if(cmd){
-    var childProcess = require("child_process");
-    var exec = childProcess.exec;
+  // 添加命令
+  if('string' === typeof argv['a']){
+    var cmdKey = argv['a'], cmdValue = argv['v'];
 
-    console.log("cmd get [ " + cmd + " ]");
+    if(!(cmdKey&&cmdValue)){
+      return console.log('cmd-run add cmd fail: ', cmdKey+':'+cmdValue);
+    }
 
-    // 以@:开头的都是使用node执行的文件，不是直接执行的命令
-    // @:<fileRelative>
-    if('@:' !== cmd.slice(0,2)){
-      var child = exec(cmd, function(err, stdout, stderr){
-        if(err){
+    config[cmdKey] = cmdValue;
+
+    console.log('cmd-run add cmd: ', cmdKey+':'+cmdValue);
+
+    writeConfig(config);
+  }
+
+  // 删除命令
+  if('string' === typeof argv['d']){
+    var cmdKey = argv['d'], cmdValue = config[cmdKey];
+
+    if(!(cmdKey&&cmdValue)){
+      return console.log('cmd-run delete cmd fail: ', cmdKey+':'+cmdValue);
+    }
+
+    delete config[cmdKey];
+
+    console.log('cmd-run delete cmd: ', cmdKey+':'+cmdValue);
+
+    writeConfig(config);
+  }
+
+  // 列出查看
+  if(argv['l']){
+    var arrConfigKeyValues = [];
+    for(var key in config){
+      arrConfigKeyValues.push(key+': '+config[key]);
+    }
+    console.log(arrConfigKeyValues.join('\n'));
+  }
+}
+
+/** 运行命令 */
+function cmdRun(){
+  var arrCmd = argv['_'] || [];
+  arrCmd.forEach(function(cmdKey){
+    var cmd = config[cmdKey] || '';
+
+    if('-' === cmd.slice(0,1)){
+      return;
+    }
+
+    if(cmd){
+      var childProcess = require("child_process");
+      var exec = childProcess.exec;
+
+      console.log("cmd get [ " + cmd + " ]");
+
+      // 以@:开头的都是使用node执行的文件，不是直接执行的命令
+      // @:<fileRelative>
+      if('@:' !== cmd.slice(0,2)){
+        var child = exec(cmd, function(err, stdout, stderr){
+          if(err){
+            return console.log("cmd [ "+ cmd +" ] err: " + err);
+          }
+
+          console.log("cmd [ " + cmd + " ] success");
+        });
+      }else{
+        var fileRelative = cmd.slice(2),
+          fileAbsolute = path.join(__dirname, fileRelative);
+
+        var script = require(fileAbsolute);
+
+        try{
+          script();
+        }catch(err){
           return console.log("cmd [ "+ cmd +" ] err: " + err);
         }
-
-        console.log("cmd [ " + cmd + " ] success");
-      });
-    }else{
-      var fileRelative = cmd.slice(2),
-        fileAbsolute = path.join(__dirname, fileRelative);
-
-      var script = require(fileAbsolute);
-
-      try{
-        script();
-      }catch(err){
-        return console.log("cmd [ "+ cmd +" ] err: " + err);
       }
+
+      switch(cmdKey){
+        case 'http-server':
+        case 'hs':
+          openUrl('http://127.0.0.1:8001/', 8001);
+          break;
+      }
+
+    }
+  });
+}
+
+/** 写入配置config并生成文件 */
+function writeConfig(configObj){
+  fs.writeFile(configFilePathFull, JSON.stringify(configObj), function(err){
+    if(err){
+      return console.log('writeConfig err: ', err);
     }
 
-    switch(cmdKey){
-      case 'http-server':
-      case 'hs':
-        openUrl('http://127.0.0.1:8001/', 8001);
-        break;
-    }
+    console.log('writeConfig success!');
+  });
+}
 
-  }
-});
+/** 读取配置 */
+function getConfig(){
+  return require(configFilePathFull);
+}
 
-
-/**
- * 打开浏览器
- */
+/** 打开浏览器 */
 function openUrl (path, callback) {
   var IS_WIN = process.platform.indexOf('win') === 0;
   var escapeShellArg = function(cmd) {
@@ -78,3 +152,4 @@ function openUrl (path, callback) {
   }
   child_process.exec(cmd, callback);
 }
+
